30c990ebce6c8c70f783d400d8e9c10a4300b111
devc3tt <104304061+devc3tt@users.noreply.github.com>
2026-08-03 14:07:48 +0200

    devShell: remove unneeded and bunjs

--- parts/shell.nix
+++ parts/shell.nix
@@ -1,27 +1,22 @@
-{ inputs, ... }: {
-  perSystem =
-    {
-      pkgs,
-      system,
-      ...
-    }:
-    {
-      _module.args.pkgs = import inputs.nixpkgs {
-        inherit system;
-        overlays = [
-          inputs.rust-overlay.overlays.default
-        ];
-      };
-      devShells.default = pkgs.mkShell {
-        packages = [
-          # todo make it actually work
-          # HOLD UP I'M STUPID: the thing is, the extensions are all in rust docs
-          (pkgs.rust-bin.stable.latest.default.override {
-            extensions = [ "rust-analyzer" ];
-          })
-          pkgs.kdePackages.qtdeclarative
-          pkgs.lldb
-        ];
-      };
+{inputs, ...}: {
+  perSystem = {
+    pkgs,
+    system,
+    ...
+  }: {
+    _module.args.pkgs = import inputs.nixpkgs {
+      inherit system;
+      overlays = [
+        inputs.rust-overlay.overlays.default
+      ];
     };
+    devShells.default = pkgs.mkShell {
+      packages = [
+        (pkgs.rust-bin.stable.latest.default.override {
+          extensions = ["rust-analyzer"];
+        })
+        pkgs.bun
+      ];
+    };
+  };
 }
